959ed30ed3c75f10758b77b840ef749096a311c0
Steve Potts <coolpot_1@yahoo.com>
2021-01-18 17:00:09 +0000

    initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log('Something' + ' is better than nothing');
+
+console.log('Steve' + 'purplebeard');
+
+const numbers = [6, 38, 15, 33, 999];
+
+numbers.forEach(el => {
+  console.log(el % 2 === 0);
+})
+
+console.log(typeof(5));
